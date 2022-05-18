2652db6c0fc356bb8ea6effcc937288139ed5567
Piotr Rogula <piotr.r99@gmail.com>
2022-05-18 09:54:52 +0200

    The rest is done in Bogna's repo

---
+++ list_5/models/__init__.py
@@ -0,0 +1,11 @@
+from list_5.models.sir_model import total_infected_vs_r0, SIR_visualiser, phase_portrait_visualiser, solver, si_model,\
+    sir_model
+
+__all__ = [total_infected_vs_r0,
+           SIR_visualiser,
+           phase_portrait_visualiser,
+           solver,
+           si_model,
+           sir_model]
+
+
